bd6d44b53d60a6b1bb13259f12c1ea549a969333
Kaushal Kishor Rauniyar <157037723+kaushalxluciferr@users.noreply.github.com>
2024-06-09 11:21:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputbox=document.querySelector("#input-box");
+const listcontainer=document.querySelector("#list-container");
+
+function Addtask()
+{
+    if(inputbox.value=="")
+        {
+            alert("You must write something");
+        }
+        else{
+            let li=document.createElement("li");
+            li.innerHTML=inputbox.value;
+            listcontainer.appendChild(li);
+            let span=document.createElement("span");
+            span.innerHTML="\u00d7";
+            li.appendChild(span);
+        }
+        inputbox.value="";
+        savedata();
+}
+
+listcontainer.addEventListener("click",(e)=>{
+    if(e.target.tagName=="LI")
+        {
+            e.target.classList.toggle("checked");
+            savedata();
+        }
+        else if(e.target.tagName=="SPAN")
+        {
+    e.target.parentElement.remove();
+    savedata();
+        }
+},false)
+
+function savedata()
+{
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showtask()
+{
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+
+showtask();
